fb157448e9a8d9233e2f47150f216e9e14ab4f97
Marc Otten <marcotten1@gmail.com>
2012-03-09 22:35:58 +0100

    Devise eingerichtet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Tipp5::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #Devise Mailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
 end

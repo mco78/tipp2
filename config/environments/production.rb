24f36984da537446bda55ba25416785e3b605d8c
Marc Otten <marcotten1@gmail.com>
2012-03-16 00:01:47 +0100

    set config.assets.compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Tipp5::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  ## set to true for heroku deployment
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true

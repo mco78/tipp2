62c60ca1149734e77fcf68baf16454ea05f63134
Marc Otten <marcotten1@gmail.com>
2012-06-02 00:06:13 +0200

    added editable user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
  # -*- coding: utf-8 -*-
 class UsersController < ApplicationController
 
-	before_filter :authenticate_admin!
+	before_filter :authenticate_admin!, :only => [:index, :destroy]
 	
 	def index
 		@title = "Übersicht User"
 		@users = User.all.sort_by { |u| [u.last_sign_in_at ? 1 : 0, u.last_sign_in_at] }
 	end
-
 
 	def destroy
 		User.find(params[:id]).destroy

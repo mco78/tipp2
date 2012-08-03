ac9e2c65bfee055c4f0a41c2431f7bbed715061f
Marc Otten <marcotten1@gmail.com>
2012-08-03 23:27:24 +0200

    removed unnessecary migrations that caused trouble on heroku

--- db/migrate/20120713193537_add_community_admin_to_communites.rb
+++
@@ -1,6 +0,0 @@
-class AddCommunityAdminToCommunites < ActiveRecord::Migration
-  def change
-    add_column :communities, :community_admin, :integer
-
-  end
-end
--- db/migrate/20120713193732_remove_column_community_admin.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnCommunityAdmin < ActiveRecord::Migration
-  def change
-    remove_column :communities, :community_admin, :integer
-
-  end
-end

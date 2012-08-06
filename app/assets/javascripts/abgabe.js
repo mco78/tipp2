60a4c026f0c81bc3f968e3a50eedd72ed1600224
Marc Otten <marcotten1@gmail.com>
2012-08-06 23:34:57 +0200

    added color effect to js updates at abgabe

--- app/assets/javascripts/abgabe.js
+++ app/assets/javascripts/abgabe.js
@@ -1,4 +1,14 @@
 $('.submittable').live('change', function() {
      $(this).parents('form:first').submit();
+     //$(this).css('color', 'red');
+     $(this).animate({
+     	color: 'red',
+  	}, '100', function() {
+  }).delay(400);
+     $(this).animate({
+     	color: 'black'
+  	}, '800', function() {
+    	
+  });
 });
 

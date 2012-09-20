a5379d2d3a18e71c78188c047641c322c56c139b
Marc Otten <marcotten1@gmail.com>
2012-09-20 23:21:56 +0200

    changed host in setup_mail.rb to tipp2.net

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -8,6 +8,6 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "tipp2.net"
 require 'development_mail_interceptor' 
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?

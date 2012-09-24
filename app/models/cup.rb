01d2dc4bb43e663b549a651fa023f08ff489888d
Marc Otten <marcotten1@gmail.com>
2012-09-24 22:12:01 +0200

    enabled messages on fix_current_results again

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -46,15 +46,15 @@ class Cup < ActiveRecord::Base
 
 	def fix_current_results
 		cup = self
-		#@messages = []
-		#@messages << "Starte Ergebniseintragung aktueller Spieltag"
+		@messages = []
+		@messages << "Starte Ergebniseintragung aktueller Spieltag"
 		client = Savon::Client.new("http://www.openligadb.de/Webservices/Sportsdata.asmx?WSDL")
 		response = client.request :web, :get_current_group, body: { "leagueShortcut" => cup.league_shortcut}
 		data = data = response.to_hash[:get_current_group_response] [:get_current_group_result]
 		
 		current_round = Round.find_by_open_liga_id(data[:group_id])
 		fix_results(cup, current_round)
-		#return @messages
+		return @messages
 	end
 
 	def fix_certain_results(current_round)
@@ -66,7 +66,7 @@ class Cup < ActiveRecord::Base
 	end
 
 	def fix_results(cup, current_round)
-		#@messages << "Wettbewerb: " + cup.name + ", Runde: " + current_round.name + " (Leg: " + current_round.leg.to_s + ", ID: " + current_round.id.to_s + ")"
+		@messages << "Wettbewerb: " + cup.name + ", Runde: " + current_round.name + " (Leg: " + current_round.leg.to_s + ", ID: " + current_round.id.to_s + ")"
 		client = Savon::Client.new("http://www.openligadb.de/Webservices/Sportsdata.asmx?WSDL")
 		response = client.request :web, :get_matchdata_by_group_league_saison, 
 			body: { "groupOrderID" => current_round.leg, "leagueShortcut" => cup.league_shortcut, "leagueSaison" => cup.league_season}
@@ -75,15 +75,15 @@ class Cup < ActiveRecord::Base
 		data.each do |match|
 			game = Game.find_by_open_liga_id(match[:match_id])
 			if game.nil?
-				#@messages << "Spiel in der Datenbank nicht gefunden. Keine Aktion durchgeführt."
+				@messages << "Spiel in der Datenbank nicht gefunden. Keine Aktion durchgeführt."
 			else
-				#@messages << Team.find(game.home_team).acronym + " - " + Team.find(game.away_team).acronym
+				@messages << Team.find(game.home_team).acronym + " - " + Team.find(game.away_team).acronym
 				if match[:match_is_finished] === true
 					if game.home_score.nil?
 						game.home_score = match[:points_team1]
 						game.away_score = match[:points_team2]
 						game.save
-						#@messages << "Ergebnis eingetragen: " + game.home_score.to_s + ":" + game.away_score.to_s
+						@messages << "Ergebnis eingetragen: " + game.home_score.to_s + ":" + game.away_score.to_s
 						
 						bets = Bet.find_all_by_game_id(game.id)
 						bet_count = bets.count
@@ -99,7 +99,7 @@ class Cup < ActiveRecord::Base
 						#if bet_count === bets_fixed
 						#	@messages << "Punkte bei " + bets_fixed + " Tipps verteilt."
 						#else
-							#@messages << "Punkte bei " + bets_fixed.to_s + " von " + bet_count.to_s + " gefundenen Tipps verteilt."
+							@messages << "Punkte bei " + bets_fixed.to_s + " von " + bet_count.to_s + " gefundenen Tipps verteilt."
 						#end
 
 						if round_over(game) === true
@@ -108,14 +108,14 @@ class Cup < ActiveRecord::Base
 
 					else
 						if match[:points_team1].to_i == game.home_score &&  match[:points_team2].to_i == game.away_score
-							#@messages << "Ergebnis ist bereits eingetragen."
+							@messages << "Ergebnis ist bereits eingetragen."
 						else
-							#@messages << "ACHTUNG: Spiel in Datenbank hat bereits Ergebnis, welches von dem Open-Liga-DB-Ergebnis abweicht! Manuell überprüfen!"
+							@messages << "ACHTUNG: Spiel in Datenbank hat bereits Ergebnis, welches von dem Open-Liga-DB-Ergebnis abweicht! Manuell überprüfen!"
 							# Nachricht über Konflikt an Admin
 						end
 					end
 				else
-					#@messages << "Spiel noch nicht beendet."
+					@messages << "Spiel noch nicht beendet."
 				end
 			end
 		end
